96b9f8a4279ea541e66c37e1bd25ce2ffa6dd067
user <user@Baraa>
2023-11-14 23:39:50 +0100

    modifier

--- Tp0Poo/src/tp0/HelloWorld.java
+++ Tp0Poo/src/tp0/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Je suis une version modifiée du code de l'exo 1");
 	}
 
 }
